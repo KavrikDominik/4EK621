95d2981ecb786e128c77a45b6927bcdf99a0cb90
Dominik Kavrik <dominik@Dominik-MacBook-Pro.local>
2023-03-07 15:07:22 +0100

    develop: updating Rsquared simulation to ENG

--- week_3/Simulace_R_squared.R
+++ week_3/Simulace_R_squared.R
@@ -1,31 +1,35 @@
-# Simulace nešvaru koeficientu determinace  -------------------------------
-set.seed(155)
-N <- 100
-C <- c()
-D <- c()
-CC <- list()
-const <- cbind(rep(1, N)) # vektor jednicek
-X <- cbind(const) # matice X zatím pouze s jednickami
-Y <- rnorm(N, 2, 2) # vysvětovaná proměnná
-# od jednoho náhodného regresorů az po 100 regresorů:
+# Simulation of the issue with R squared and # of regressors  ------------------
+set.seed(155) # seed for random number generator
+N <- 100  
+C <- c() # empty vector initialization
+D <- c() # empty vector initialization
+CC <- list() # empty list initialization
+const <- cbind(rep(1, N)) # column vector of ones for design matrix X
+X <- cbind(const) # matrix X
+Y <- rnorm(N, 2, 2) # column vectrr of dependent variable
 
+# now we iterate from 1 to 99 and in each iteration we a new regressor 
 for (i in 1:99) {
-  xi <- rnorm(N, 1, 2) # nový regresor
-  X <- cbind(X, xi) # přidáme do matice X
-  k <- ncol(X) - 1 # počet regresorů bez konstanty
-  beta <- solve(t(X) %*% X) %*% t(X) %*% Y
-  Y_hat <- X %*% beta
-  SSR <- sum((Y_hat - Y)^2)
-  SST <- sum((Y - mean(Y))^2)
-  SSE <- SST - SSR
-  R_squared <- SSE / SST
-  R_adj <- 1 - (((1 - R_squared) * (N - 1)) / (N - k - 1))
-  print(i)
+  xi <- rnorm(N, 1, 2) # new regressor
+  X <- cbind(X, xi) # add new regressor to the design matrix X
+  k <- ncol(X) - 1 # number of regressors without the constant term
+  beta <- solve(t(X) %*% X) %*% t(X) %*% Y # run OLS
+  Y_hat <- X %*% beta # obtain fitted values
+  SSR <- sum((Y_hat - Y)^2) # calculate sum of squared residuals 
+  SST <- sum((Y - mean(Y))^2) # calculate the sum of squares total
+  SSE <- SST - SSR # get explained sum of squares
+  R_squared <- SSE / SST # calculate R squared   
+  R_adj <- 1 - (((1 - R_squared) * (N - 1)) / (N - k - 1)) #...and andjusted R^2
+  print(i) 
+
+  # now save everything to intialized objects  
   C[i] <- R_squared
   D[i] <- R_adj
   CC[["R_squared"]][i] <- R_squared
   CC[["R_adjusted"]][i] <- R_adj
 }
+
+# Plotting the results: ---------------------------------------------------
 
 par(
   mfrow = c(2, 1),
